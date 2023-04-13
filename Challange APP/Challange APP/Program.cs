8cf75451a72ff2ea92937e6c67ec85a1195d8f92
Mateusz Nowakowski <mateusznowakowski1a@gmail.com>
2023-04-13 23:16:05 +0200

    Praca Domowa nr 1

--- Challange APP/Challange APP/Program.cs
+++ Challange APP/Challange APP/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Lat = 25;
+string Name = ("Mati");
+string sex = ("Men");
+
+if (sex != "Men" && Lat < 25)
+{
+    Console.WriteLine("Kobieta poniżej 25 lat");
+}
+else if (Name == "Mati" && Lat == 25)
+{
+    Console.WriteLine("Mati, lat 25!");
+}
+else if (sex == "Men" && Lat > 25)
+{
+    Console.WriteLine("Meżczyzna powyżej lat 25");
+}
